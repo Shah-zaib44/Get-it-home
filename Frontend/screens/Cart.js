52ff6eecb582b118b0c711c9e8e53428a94c61b9
ahmed0336 <balochahmed823@gmail.com>
2022-02-06 02:34:08 +0500

    cart page  remaing work

--- Frontend/screens/Cart.js
+++ Frontend/screens/Cart.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Text, View, Image, TouchableOpacity} from 'react-native';
+import { Text, View, Image, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 // import { Button } from 'react-native-paper';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import {Button} from 'react-native-paper';
+import { Button } from 'react-native-paper';
 
 const Cart = () => {
   return (
-    <View
+    <ScrollView
       style={{
         backgroundColor: '#fff',
         flex: 1,
@@ -39,13 +39,15 @@ const Cart = () => {
           />
         </View>
 
-        <View style={{flex: 1}}>
-          <View style={{flexDirection: 'row'}}>
-            <View style={{flexGrow: 1, marginStart: 10}}>
-              <Text style={{color: 'black', paddingBottom: 10}}>Nike Air</Text>
-              <Text style={{color: 'black', paddingBottom: 8}}>36 Miami</Text>
+        <View style={{ flex: 1 }}>
+          <View style={{ flexDirection: 'row' }}>
+
+            <View style={{ flexGrow: 1, marginStart: 10 }}>
+              <Text style={{ color: 'black', paddingBottom: 10 }}>Nike Air</Text>
+              <Text style={{ color: 'black', paddingBottom: 8 }}>36 Miami</Text>
             </View>
-            <View style={{flexDirection: 'row'}}>
+
+            <View style={{ flexDirection: 'row' }}>
               <TouchableOpacity>
                 <View
                   style={{
@@ -57,7 +59,7 @@ const Cart = () => {
                   <FontAwesome5
                     name={'heart'}
                     size={20}
-                    style={{color: 'black'}}
+                    style={{ color: 'black' }}
                   />
                 </View>
               </TouchableOpacity>
@@ -73,13 +75,14 @@ const Cart = () => {
                   <FontAwesome5
                     name={'trash'}
                     size={18}
-                    style={{color: 'black'}}
+                    style={{ color: 'black' }}
                   />
                 </View>
               </TouchableOpacity>
             </View>
           </View>
-          <View style={{flexDirection: 'row'}}>
+
+          <View style={{ flexDirection: 'row' }}>
             <Text
               style={{
                 flexGrow: 1,
@@ -91,9 +94,9 @@ const Cart = () => {
               $10
             </Text>
 
-            <View style={{flexDirection: 'row'}}>
+            <View style={{ flexDirection: 'row' }}>
               <TouchableOpacity
-                style={{borderWidth: 1, borderColor: 'gray', borderRadius: 5}}>
+                style={{ borderWidth: 1, borderColor: 'gray', borderRadius: 5 }}>
                 <View
                   style={{
                     paddingHorizontal: 10,
@@ -104,7 +107,7 @@ const Cart = () => {
                   <FontAwesome5
                     name={'plus'}
                     size={14}
-                    style={{color: 'gray'}}
+                    style={{ color: 'gray' }}
                   />
                 </View>
               </TouchableOpacity>
@@ -118,11 +121,11 @@ const Cart = () => {
                   backgroundColor: 'gray',
                   borderRadius: 5,
                 }}>
-                <Text style={{color: '#fff'}}>Num</Text>
+                <Text style={{ color: '#fff' }}>Num</Text>
               </View>
 
               <TouchableOpacity
-                style={{borderWidth: 1, borderColor: 'gray', borderRadius: 5}}>
+                style={{ borderWidth: 1, borderColor: 'gray', borderRadius: 5 }}>
                 <View
                   style={{
                     paddingHorizontal: 10,
@@ -133,13 +136,16 @@ const Cart = () => {
                   <FontAwesome5
                     name={'minus'}
                     size={14}
-                    style={{color: 'gray'}}
+                    style={{ color: 'gray' }}
                   />
                 </View>
               </TouchableOpacity>
             </View>
           </View>
+
+
         </View>
+
       </View>
       {/* card one end  */}
 
@@ -200,13 +206,13 @@ const Cart = () => {
           </View>
           
         </View> */}
-        <View style={{flex: 1}}>
-          <View style={{flexDirection: 'row'}}>
-            <View style={{flexGrow: 1, marginStart: 10}}>
-              <Text style={{color: 'black', paddingBottom: 10}}>Nike Air</Text>
-              <Text style={{color: 'black', paddingBottom: 8}}>36 Miami</Text>
+        <View style={{ flex: 1 }}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flexGrow: 1, marginStart: 10 }}>
+              <Text style={{ color: 'black', paddingBottom: 10 }}>Nike Air</Text>
+              <Text style={{ color: 'black', paddingBottom: 8 }}>36 Miami</Text>
             </View>
-            <View style={{flexDirection: 'row'}}>
+            <View style={{ flexDirection: 'row' }}>
               <TouchableOpacity>
                 <View
                   style={{
@@ -218,7 +224,7 @@ const Cart = () => {
                   <FontAwesome5
                     name={'heart'}
                     size={20}
-                    style={{color: 'black'}}
+                    style={{ color: 'black' }}
                   />
                 </View>
               </TouchableOpacity>
@@ -233,13 +239,13 @@ const Cart = () => {
                   <FontAwesome5
                     name={'trash'}
                     size={18}
-                    style={{color: 'black'}}
+                    style={{ color: 'black' }}
                   />
                 </View>
               </TouchableOpacity>
             </View>
           </View>
-          <View style={{flexDirection: 'row'}}>
+          <View style={{ flexDirection: 'row' }}>
             <Text
               style={{
                 flexGrow: 1,
@@ -251,9 +257,9 @@ const Cart = () => {
               $10
             </Text>
 
-            <View style={{flexDirection: 'row'}}>
+            <View style={{ flexDirection: 'row' }}>
               <TouchableOpacity
-                style={{borderWidth: 1, borderColor: 'gray', borderRadius: 5}}>
+                style={{ borderWidth: 1, borderColor: 'gray', borderRadius: 5 }}>
                 <View
                   style={{
                     paddingHorizontal: 10,
@@ -264,7 +270,7 @@ const Cart = () => {
                   <FontAwesome5
                     name={'plus'}
                     size={14}
-                    style={{color: 'gray'}}
+                    style={{ color: 'gray' }}
                   />
                 </View>
               </TouchableOpacity>
@@ -278,11 +284,11 @@ const Cart = () => {
                   backgroundColor: 'gray',
                   borderRadius: 5,
                 }}>
-                <Text style={{color: '#fff'}}>Num</Text>
+                <Text style={{ color: '#fff' }}>Num</Text>
               </View>
 
               <TouchableOpacity
-                style={{borderWidth: 1, borderColor: 'gray', borderRadius: 5}}>
+                style={{ borderWidth: 1, borderColor: 'gray', borderRadius: 5 }}>
                 <View
                   style={{
                     paddingHorizontal: 10,
@@ -293,7 +299,7 @@ const Cart = () => {
                   <FontAwesome5
                     name={'minus'}
                     size={14}
-                    style={{color: 'gray'}}
+                    style={{ color: 'gray' }}
                   />
                 </View>
               </TouchableOpacity>
@@ -331,13 +337,13 @@ const Cart = () => {
           />
         </View>
 
-        <View style={{flex: 1}}>
-          <View style={{flexDirection: 'row'}}>
-            <View style={{flexGrow: 1, marginStart: 10}}>
-              <Text style={{color: 'black', paddingBottom: 10}}>Nike Air</Text>
-              <Text style={{color: 'black', paddingBottom: 8}}>36 Miami</Text>
+        <View style={{ flex: 1 }}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flexGrow: 1, marginStart: 10 }}>
+              <Text style={{ color: 'black', paddingBottom: 10 }}>Nike Air</Text>
+              <Text style={{ color: 'black', paddingBottom: 8 }}>36 Miami</Text>
             </View>
-            <View style={{flexDirection: 'row'}}>
+            <View style={{ flexDirection: 'row' }}>
               <TouchableOpacity>
                 <View
                   style={{
@@ -349,7 +355,7 @@ const Cart = () => {
                   <FontAwesome5
                     name={'heart'}
                     size={20}
-                    style={{color: 'black'}}
+                    style={{ color: 'black' }}
                   />
                 </View>
               </TouchableOpacity>
@@ -364,13 +370,13 @@ const Cart = () => {
                   <FontAwesome5
                     name={'trash'}
                     size={18}
-                    style={{color: 'black'}}
+                    style={{ color: 'black' }}
                   />
                 </View>
               </TouchableOpacity>
             </View>
           </View>
-          <View style={{flexDirection: 'row'}}>
+          <View style={{ flexDirection: 'row' }}>
             <Text
               style={{
                 flexGrow: 1,
@@ -382,9 +388,9 @@ const Cart = () => {
               $10
             </Text>
 
-            <View style={{flexDirection: 'row'}}>
+            <View style={{ flexDirection: 'row' }}>
               <TouchableOpacity
-                style={{borderWidth: 1, borderColor: 'gray', borderRadius: 5}}>
+                style={{ borderWidth: 1, borderColor: 'gray', borderRadius: 5 }}>
                 <View
                   style={{
                     paddingHorizontal: 10,
@@ -395,7 +401,7 @@ const Cart = () => {
                   <FontAwesome5
                     name={'plus'}
                     size={14}
-                    style={{color: 'gray'}}
+                    style={{ color: 'gray' }}
                   />
                 </View>
               </TouchableOpacity>
@@ -409,11 +415,11 @@ const Cart = () => {
                   backgroundColor: 'gray',
                   borderRadius: 5,
                 }}>
-                <Text style={{color: '#fff'}}>Num</Text>
+                <Text style={{ color: '#fff' }}>Num</Text>
               </View>
 
               <TouchableOpacity
-                style={{borderWidth: 1, borderColor: 'gray', borderRadius: 5}}>
+                style={{ borderWidth: 1, borderColor: 'gray', borderRadius: 5 }}>
                 <View
                   style={{
                     paddingHorizontal: 10,
@@ -424,7 +430,7 @@ const Cart = () => {
                   <FontAwesome5
                     name={'minus'}
                     size={14}
-                    style={{color: 'gray'}}
+                    style={{ color: 'gray' }}
                   />
                 </View>
               </TouchableOpacity>
@@ -434,12 +440,281 @@ const Cart = () => {
       </View>
       {/* end third cart */}
 
-      {/* <View>
-      <Button icon="camera" mode="outline" onPress={() => console.log('Pressed')}>
-    Press me
-  </Button>
-      </View> */}
-    </View>
+      {/* checkout button */}
+
+      <View
+        style={{
+          // backgroundColor: '#fff',
+          marginVertical: 20,
+          marginHorizontal: 20,
+          // paddingVertical: 20,
+          paddingHorizontal: 20,
+          //  flex: 0.3,
+          backgroundColor: '#fff',
+          borderWidth: 2,
+          flexDirection: 'row',
+          marginBottom: 10,
+
+          // height: 200
+        }}>
+
+        {/* <Text style={{
+            color: '#fff', paddingBottom: 10, textAlign: 'center',
+            marginStart: 78,
+            fontSize: 20
+          }}>Apply</Text> */}
+        <TextInput
+          style={{
+            height: 40,
+            width: 209,
+            color: 'grey'
+            // borderColor: 'gray',
+            // borderWidth: 1
+          }}
+          defaultValue="Enter  Cupon  Code"
+        />
+
+        <TouchableOpacity>
+
+
+          <Text
+            style={{
+              color: 'black',
+              backgroundColor: 'blue',
+              fontSize: 29,
+              paddingHorizontal: 4,
+              paddingVertical: 4,
+              marginStart: 8
+              ,
+
+            }}
+          >Apply</Text>
+
+        </TouchableOpacity>
+
+
+
+
+
+
+
+      </View>
+
+
+      {/* new card  */}
+      <View
+        style={{
+          backgroundColor: '#fff',
+          marginVertical: 20,
+          marginHorizontal: 20,
+          paddingVertical: 20,
+          paddingHorizontal: 20,
+          //  flex: 0.3,
+          backgroundColor: '#fff',
+          borderWidth: 2,
+          flexDirection: 'row',
+          marginBottom: 10,
+          // height: 200
+        }}>
+
+
+        <View style={{ flex: 1 }}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flexGrow: 1, marginStart: 5, paddingVertical: 10 }}>
+              <Text style={{ color: 'black', paddingBottom: 10 }}>item3</Text>
+              {/* <Text style={{ color: 'black', paddingBottom: 8 }}>36 Miami</Text> */}
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+              {/* <TouchableOpacity>
+                <View
+                  style={{
+                    paddingHorizontal: 10,
+                    paddingVertical: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <FontAwesome5
+                    name={'heart'}
+                    size={20}
+                    style={{ color: 'black' }}
+                  />
+                  <Text style={{ color: 'black', paddingBottom: 10 }}>item3</Text>
+                </View>
+              </TouchableOpacity> */}
+
+              <View
+                style={{
+                  paddingHorizontal: 10,
+                  paddingVertical: 10,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                {/* <FontAwesome5
+                    name={'trash'}
+                    size={18}
+                    style={{ color: 'black' }}
+                  /> */}
+                <Text style={{ color: 'black', paddingBottom: 10 }}>$598.86</Text>
+              </View>
+
+
+
+            </View>
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flexGrow: 1, marginStart: 5, paddingVertical: 10 }}>
+              <Text style={{ color: 'black', paddingBottom: 10 }}>Shipping</Text>
+              {/* <Text style={{ color: 'black', paddingBottom: 8 }}>36 Miami</Text> */}
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+              {/* <TouchableOpacity>
+                <View
+                  style={{
+                    paddingHorizontal: 10,
+                    paddingVertical: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <FontAwesome5
+                    name={'heart'}
+                    size={20}
+                    style={{ color: 'black' }}
+                  />
+                  <Text style={{ color: 'black', paddingBottom: 10 }}>item3</Text>
+                </View>
+              </TouchableOpacity> */}
+
+              <View
+                style={{
+                  paddingHorizontal: 10,
+                  paddingVertical: 10,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                {/* <FontAwesome5
+                    name={'trash'}
+                    size={18}
+                    style={{ color: 'black' }}
+                  /> */}
+                <Text style={{ color: 'black', paddingBottom: 10 }}>$40.00</Text>
+              </View>
+
+
+
+            </View>
+          </View>
+
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flexGrow: 1, marginStart: 5, paddingVertical: 10 }}>
+              <Text style={{ color: 'black', paddingBottom: 10 }}>import charges</Text>
+              {/* <Text style={{ color: 'black', paddingBottom: 8 }}>36 Miami</Text> */}
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+              {/* <TouchableOpacity>
+                <View
+                  style={{
+                    paddingHorizontal: 10,
+                    paddingVertical: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <FontAwesome5
+                    name={'heart'}
+                    size={20}
+                    style={{ color: 'black' }}
+                  />
+                  <Text style={{ color: 'black', paddingBottom: 10 }}>item3</Text>
+                </View>
+              </TouchableOpacity> */}
+
+              <View
+                style={{
+                  paddingHorizontal: 10,
+                  paddingVertical: 10,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                {/* <FontAwesome5
+                    name={'trash'}
+                    size={18}
+                    style={{ color: 'black' }}
+                  /> */}
+                <Text style={{ color: 'black', paddingBottom: 10 }}>$128.00</Text>
+              </View>
+
+
+
+            </View>
+          </View>
+
+          <View style={{ flexDirection: 'row' }}>
+            <Text
+              style={{
+                flexGrow: 1,
+                color: 'black',
+                marginStart: 10,
+                alignSelf: 'flex-end',
+                fontWeight: 'bold',
+              }}>
+              Total Price
+            </Text>
+
+            <View style={{ flexDirection: 'row' }}>
+              <Text
+                style={{
+                  flexGrow: 1,
+                  color: 'blue',
+                  marginStart: 10,
+                  alignSelf: 'flex-end',
+                  fontWeight: 'bold',
+                }}>
+                $766.86
+              </Text>
+            </View>
+          </View>
+        </View>
+
+
+      </View>
+
+      {/* checkout button */}
+      <TouchableOpacity>
+        <View
+          style={{
+            // backgroundColor: '#fff',
+            marginVertical: 20,
+            marginHorizontal: 20,
+            paddingVertical: 20,
+            paddingHorizontal: 20,
+            //  flex: 0.3,
+            backgroundColor: 'blue',
+            borderWidth: 2,
+            flexDirection: 'row',
+            marginBottom: 10,
+
+            // height: 200
+          }}>
+
+          <Text style={{
+            color: '#fff', paddingBottom: 10, textAlign: 'center',
+            marginStart: 78,
+            fontSize: 20
+          }}>Check Out</Text>
+
+
+
+
+
+
+
+        </View>
+      </TouchableOpacity>
+
+
+
+
+
+    </ScrollView>
   );
 };
 
