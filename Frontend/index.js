211a35e02921452b0a9bff09666746541bca7272
ahmed0336 <balochahmed823@gmail.com>
2022-02-14 12:14:56 +0500

    view all page functionality

--- Frontend/index.js
+++ Frontend/index.js
@@ -7,5 +7,9 @@ import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 
 import App from './App';
+import add from './screens/Account'
+import adde from './screens/SignUp'
+import ahmed from './screens/SignIn'
+import ahmed2 from './screens/admin/AddCoupon'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => ahmed2);
